9cd0baa806db24087fe13b38521c8ce029c8446c
mohamedcj <mohamedcjdev@gmail.com>
2016-10-10 08:42:37 -0400

    First commit

---
+++ githubHello/src/githubHello/GitHubHello.java
@@ -0,0 +1,12 @@
+package githubHello;
+
+public class GitHubHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Helllo world!");
+
+	}
+
+}
