dababafbd9c5cadad672b8309bfe2d9a8d8e1f35
mohamedcj <mohamedcjdev@gmail.com>
2016-10-10 19:32:05 -0400

    Preparing to part tow (conflict and merg)

--- githubHello/src/githubHello/GitHubHello.java
+++ githubHello/src/githubHello/GitHubHello.java
@@ -3,9 +3,13 @@ package githubHello;
 public class GitHubHello {
 
 	public static void main(String[] args) {
+		String[] names = {"Abdessalam","Maryem", "Selsabil", "Sirine" };
 
-		// a greeting msg
-		System.out.println("Helllo world!");
+		// a greeting msg 
+		for(int i=0; i < names.length; i++){
+			System.out.println("Helllo world. My name is: " + names[i]);
+		}
+		
 
 	}
 
