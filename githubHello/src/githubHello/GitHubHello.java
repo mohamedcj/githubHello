6a5ccfa9851d4ee146d34ce2e1d1b64a33329dc0
meddjerc <mohamedcjdev@gmail.com>
2016-10-10 19:58:30 -0400

    ubuntu-Ajout de deux noms et changement de la structure for.

--- githubHello/src/githubHello/GitHubHello.java
+++ githubHello/src/githubHello/GitHubHello.java
@@ -3,11 +3,11 @@ package githubHello;
 public class GitHubHello {
 
 	public static void main(String[] args) {
-		String[] names = {"Abdessalam","Maryem", "Selsabil", "Sirine" };
+		String[] names = {"Abdessalam","Maryem", "Selsabil", "Sirine", "Beya", "Yacine", "Sara" };
 
 		// a greeting msg 
-		for(int i=0; i < names.length; i++){
-			System.out.println("Helllo world. My name is: " + names[i]);
+		for(String name : names){
+			System.out.println("Helllo world. My name is: " + name);
 		}
 		
 
