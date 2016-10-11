4b65e5a63eb7cd5d6c0895103d72b42b574a1f66
mohamedjerbi <mohamedjerbi@192.168.31.128>
2016-10-10 20:05:41 -0400

    mac-ajout d'un sysout et ajout de deux noms.

--- githubHello/src/githubHello/GitHubHello.java
+++ githubHello/src/githubHello/GitHubHello.java
@@ -3,11 +3,12 @@ package githubHello;
 public class GitHubHello {
 
 	public static void main(String[] args) {
-		String[] names = {"Abdessalam","Maryem", "Selsabil", "Sirine" };
+		String[] names = {"Abdessalam","Maryem", "Selsabil", "Sirine","Mohamed Aziz", "Sophia" };
 
-		// a greeting msg 
+		// a greeting msg pour les bons amis !!!
 		for(int i=0; i < names.length; i++){
 			System.out.println("Helllo world. My name is: " + names[i]);
+			System.out.println("How ate you today?");
 		}
 		
 
