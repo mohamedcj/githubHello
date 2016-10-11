57fe2f8dfa221d9422e1cefdf815f0b1b7ae46cf
meddjerc <mohamedcjdev@gmail.com>
2016-10-11 08:40:47 -0400

    mac-changements dans names et les message sysout

--- githubHello/src/githubHello/GitHubHello.java
+++ githubHello/src/githubHello/GitHubHello.java
@@ -3,11 +3,11 @@ package githubHello;
 public class GitHubHello {
 
 	public static void main(String[] args) {
-		String[] names = {"Abdessalam","Maryem", "Selsabil", "Sirine", "Beya", "Yacine", "Sara", "Mohamed Aziz", "Sophia" };
+		String[] names = {"Abdessalam","Maryem", "Selsabil", "Sirine", "Beya", "Yacine", "Sara", "Mohammed Aziz", "Sophia" };
 
 		// a greeting msg pour les bons amis !!!
 		for(String name : names){
-			System.out.println("Helllo world. My name is: " + name);
+			System.out.println("Hi my name is: " + name);
 			System.out.println("How ate you today?");
 			System.out.println(aplus());
 		}
